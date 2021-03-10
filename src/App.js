9102116ed319ebb8c4f17ca9e1bf807b0fb567c3
Matthew Staton <malanstaton@gmail.com>
2021-03-10 14:06:07 -0500

    animation to hover add on

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from "react";
 import Landing from './Landing';
-import Navigation from './Navigation';
-
-import "./styles.css";
 
 
-const e1 = document.getElementById("e1_2");
 
 export default function App() {
   
   return (
     <div className="App">
-        <Navigation />
         <Landing />
     </div>
   );
--- src/Landing.js
+++ src/Landing.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useReducer } from 'react'
-import "./styles.css"
 import video1 from './images/FrontEndSlide.mp4'
+import './styles.css';
 import { isConstructorDeclaration, reduceEachTrailingCommentRange } from 'typescript'
 
 export default function Landing() {
@@ -11,14 +11,9 @@ export default function Landing() {
                 <div className="flip-container">
                         <div className="flip-card">
                             <div className="sideA slide_1">M</div>
-                            <div className="sideB slide_1_back"></div>
-                        </div>
-                </div>
-                <div className="flip-container">
-                        <div className="flip-card">
-                            <div className="sideA slide_2">A</div>
-                                <video onClick={()=>{window.scrollTo({top: 1000, left: 100, behavior: "smooth"})}} 
-                                    onMouseOver={event=>{
+                            <video
+                                    // play video on mouse over/enter
+                                    onMouseEnter={event=>{
                                     event.target.play();
                                     console.log("MOUSE OVER DETECTED")
                                 }}  onMouseOut={
@@ -27,7 +22,28 @@ export default function Landing() {
                                         event.target.pause();
                                         event.target.currentTime = 0; 
                                     }
-                                    } type='video/mp4' src={video1} className="sideB slide_2_back">
+                                    } type='video/mp4' src={video1} style={{ minWidth: '100%',
+                                        width: '100%'}} className="sideB slide_1_back">
+                                    
+                                </video>
+                        </div>
+                </div>
+                <div className="flip-container">
+                        <div className="flip-card">
+                            <div className="sideA slide_2">A</div>
+                                <video
+                                    // play video on mouse over/enter
+                                    onMouseEnter={event=>{
+                                    event.target.play();
+                                    console.log("MOUSE OVER DETECTED")
+                                }}  onMouseOut={
+                                    // Resetting video and pausing upon mouse out.
+                                    event=>{
+                                        event.target.pause();
+                                        event.target.currentTime = 0; 
+                                    }
+                                    } type='video/mp4' src={video1} style={{ minWidth: '100%',
+                                        width: '100%'}} className="sideB slide_2_back">
                                     
                                 </video>
                         </div>
